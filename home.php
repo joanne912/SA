77b4577e1db1eb0b647033e186ec057afb4d822c
NelsonSu <nelson40514@gmail.com>
2021-04-30 17:31:02 +0800

    將CSS移成獨立檔案

---
+++ home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        function toggle2() {
+            $("#menu-wrap").animate({
+                'width': 'toggle'
+            });
+        }
+    </script>
+    <link rel="stylesheet" href="home.css" type="text/css" />
+</head>
+
+<body>
+    <div class="all">
+        <!-- 下面這個區域是上方的banner -->
+        <header>
+            <div class="menu" onclick="toggle2()">
+                <img src="img/menu.svg" alt="">
+            </div>
+            <div class="title">
+                <!-- 下面這個不會顯示出來 -->
+                <img src="img/logout.svg" alt="">
+                <!--要把登出改到彈出區域-->
+                <span>個人資料</span>
+            </div>
+            <div class="func_wrap">
+                <img src="img/bell.svg" alt="">
+                <img src="img/setting.svg" alt="">
+                <img src="img/logout.svg" alt="">
+            </div>
+        </header>
+        <!-- 下面開始是左側彈出視窗的區域 -->
+        <div id="menu-wrap">
+            <div class="sub-wrap">
+                <div class="icon">
+                    <div class="icon_circle">
+                        <img src="img/icon_boy.svg" alt="">
+                    </div>
+                    <span>title</span>
+                </div>
+                <div class="option-wrap">
+                    <a href="#">個人資訊</a>
+                    <a href="#">我的包裹</a>
+                    <a href="#">公設預約</a>
+                    <a href="#">社區公告</a>
+                    <!-- 這個位置要加登出 -->
+                </div>
+            </div>
+        </div>
+        <div class="main">
+
+        </div>
+    </div>
+</body>
+
+</html>
