d691202311c7bc5b0ad5e594b1bfd24f0d2aa96f
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-05-02 20:42:52 +0800

    調整排版&&登出功能&&session

--- login_check.php
+++ login_check.php
@@ -1,13 +1,14 @@
 <?php
-if(isset($_POST['login_btn'])){
-    $U_MAIL = $_POST['mail'];
-    $U_PASSWORD = $_POST['password'];
-    if($U_MAIL && $U_PASSWORD){
-        $_SESSION['auth'] = 5;
-        $_SESSION['id'] = 1;
-        $_SESSION['name'] = 'Amy';
-        $_SESSION['mail'] = $U_MAIL;
-        header("refresh:0;url=home.php");
+    if(isset($_POST['login_btn'])){
+        $U_MAIL = $_POST['mail'];
+        $U_PASSWORD = $_POST['password'];
+        if($U_MAIL && $U_PASSWORD){
+            session_start();
+            $_SESSION['auth'] = 5;
+            $_SESSION['id'] = 1;
+            $_SESSION['name'] = 'Amy';
+            $_SESSION['mail'] = $U_MAIL;
+            header("refresh:0;url=home.php");
+        }
     }
-}
 ?>
