0cc181252f21ad4193ef05ce612d46a4e2d4f68a
joanne912 <83213125+joanne912@users.noreply.github.com>
2021-05-03 22:45:11 +0800

    公告微調

--- announcement.php
+++ announcement.php
@@ -34,8 +34,8 @@
         .item_info{
             display: flex;
             align-items: center;
-            width: 90%;
-            margin: auto;
+            /* width: 90%;
+            margin: auto; */
             padding: .5em;
             box-sizing: border-box;
         }
@@ -68,14 +68,14 @@
         }    
         .item_profile{
             width: 70%;
-            margin-left: 6%;
+            margin-left: 1.5%;
             margin-top: 1.5em;
             /* display: -webkit-box;
             -webkit-line-clamp: 4;
             -webkit-box-orient: vertical; */
         }
         .item_img{
-            width: 90%;
+            width: 100%;
             /* background-color: #f7f7fa; */
             margin-top: 2em;
             margin: auto;
@@ -96,6 +96,39 @@
         .carousel-control-next-icon{
             background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='black' width='8' height='8' viewBox='0 0 8 8'%3e%3cpath d='M2.75 0l-1.5 1.5L3.75 4l-2.5 2.5L2.75 8l4-4-4-4z'/%3e%3c/svg%3e")
         }
+        .mes_icon{
+            width: 20%;
+            /* background-color: #ffa; */
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        .mes_icon img{
+            width: 1.75em;
+            height: 1.75em;
+        }
+        .mes_private img{
+            width: 1.75em;
+            height: 1.75em;
+        }
+        .container{
+            width: 90%;
+            margin: auto;
+        }
+        .icon_wrap{
+            /* width: 95%; */
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin: auto;
+            margin-top: 1.1em;
+        }
+        .message_wrap p{
+            margin-top: 1em;
+            letter-spacing: 5px;
+            text-indent: 5px;
+            font-size: 1em;
+        }
         @media screen and (min-width: 768px;){
             .item_profile{
                 width: 100vw;
@@ -112,53 +145,68 @@
         <input type="checkbox" name="" id="">
         <p>項目：活動</p>
     </div>
-    <div class="item_info">
-        <div class="logo">
-            <img src="img/icon_boy.svg" alt="">
-        </div>
-        <div class="info">
-            <p>名字 or 社團名稱</p>
-            <div class="sub_info">
-                <img src="img/calender.svg" alt="">
-                <p>2021.05.01</p>
-                <img src="img/location.svg" alt="">
-                <p>location</p>
+    <div class="container">
+        <div class="item_info">
+            <div class="logo">
+                <img src="img/icon_boy.svg" alt="">
+            </div>
+            <div class="info">
+                <p>名字 or 社團名稱</p>
+                <div class="sub_info">
+                    <img src="img/calender.svg" alt="">
+                    <p>2021.05.01</p>
+                    <img src="img/location.svg" alt="">
+                    <p>location</p>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="item_profile">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi voluptatibus eveniet nobis sit numquam praesentium architecto nihil ipsum molestias. Distinctio facilis quos culpa perferendis architecto vero cumque, officia odit tempore?
-        Ratione nisi necessitatibus officia consequuntur labore animi aspernatur in neque quidem amet error nulla dolorum aliquid eos sunt assumenda quisquam, quia maxime asperiores voluptate dolor ullam eveniet debitis. Unde, ipsum.
-        Natus, magni! Beatae numquam praesentium dolorem quia doloribus eum obcaecati, repellat culpa iure, rerum, sit saepe mollitia distinctio similique corrupti esse commodi aperiam nisi consequatur earum officia. Accusantium, minus nihil.</p>
-    </div>
-    <div class="item_img">
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img class="d-block w-100" src="img/001.jpeg" alt="First slide">
-            </div>
-            <div class="carousel-item">
-            <img class="d-block w-100" src="img/001.jpeg" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-            <img class="d-block w-100" src="img/001.jpeg" alt="Third slide">
-            </div>
+        <div class="item_profile">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi voluptatibus eveniet nobis sit numquam praesentium architecto nihil ipsum molestias. Distinctio facilis quos culpa perferendis architecto vero cumque, officia odit tempore?
+            Ratione nisi necessitatibus officia consequuntur labore animi aspernatur in neque quidem amet error nulla dolorum aliquid eos sunt assumenda quisquam, quia maxime asperiores voluptate dolor ullam eveniet debitis. Unde, ipsum.
+            Natus, magni! Beatae numquam praesentium dolorem quia doloribus eum obcaecati, repellat culpa iure, rerum, sit saepe mollitia distinctio similique corrupti esse commodi aperiam nisi consequatur earum officia. Accusantium, minus nihil.</p>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-
+        <div class="item_img">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img class="d-block w-100" src="img/001.jpeg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="img/001.jpeg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="img/001.jpeg" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        </div>
+        <div class="message_wrap">
+            <div class="icon_wrap">
+                <div class="mes_icon">
+                    <a href="#"><img src="img/thumb.svg" alt=""></a>
+                    <a href="#"><img src="img/message.svg" alt=""></a>
+                    <a href="#"><img src="img/reply.svg" alt=""></a>
+                </div>
+                <div class="mes_private">
+                    <a href="#"><img src="img/messenger.svg" alt=""></a>
+                </div>
+            </div>
+            <p>xxx和其他人都說讚</p>
+        </div>
+        
     </div>
 </body>
 </html>
