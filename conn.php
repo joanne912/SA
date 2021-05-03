6de82b47b597e64ad99893472220ffb3c31dce23
NelsonSu <nelson40514@gmail.com>
2021-05-03 19:35:56 +0800

    程式模組化

--- conn.php
+++ conn.php
@@ -3,10 +3,11 @@
     $dbname = "";
     $username = "";
     $password = "";
-
+    
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password);
         
+        //將錯誤報告設定為拋出exceptions異常
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
         return $conn;
---
+++ header.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="sub">
+        <div class="menu" onclick="toggle2()">
+            <img src="img/menu.svg" alt="">
+        </div>
+        <div class="title">
+            <!-- 下面這個不會顯示出來 -->
+            <img src="img/logout.svg" alt="">
+            <!--要把登出改到彈出區域-->
+            <span>個人資料</span>
+        </div>
+    </div>
+    <!-- 網頁版的設定、登出、通知按鈕，手機版時會消失 -->
+    <div class="func_wrap">
+        <a href="#">setting</a>
+        <a href="logout.php">logout</a>
+        <img src="img/bell.svg" alt="">
+    </div>
+</header>
