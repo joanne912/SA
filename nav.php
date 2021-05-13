2d2c31ac95f2ddb2af6d9c4dabd7416fe39903cc
NelsonSu <nelson40514@gmail.com>
2021-05-13 10:51:15 +0800

    開權限

--- nav.php
+++ nav.php
@@ -1,3 +1,6 @@
+<?php
+    if ($auth <= 4){
+?>
 <div id="menu-wrap">
     <div class="sub-wrap">
         <div class="icon">
@@ -20,3 +23,31 @@
         </div>
     </div>
 </div>
+<?php
+    } else if ($auth == 5){
+?>
+<div id="menu-wrap">
+    <div class="sub-wrap">
+        <div class="icon">
+            <div class="icon_circle">
+                <img src="img/icon_boy.svg" alt="">
+            </div>
+            <span>title</span>
+        </div>
+        <div class="option-wrap">
+            <a href="home.php?page=personal">個人資訊</a>
+            <a href="home.php?page=package">我的包裹</a>
+            <a href="home.php?page=order">公設預約</a>
+            <a href="home.php?page=announ">社區公告</a>
+            <!-- 這個位置要加登出 -->
+            <!-- 手機版時設定、登出、通知的按鈕位置，網頁版時會消失 -->
+            <div class="else">
+                <a href="#"><img src="img/setting.svg" alt=""></a>
+                <a href="logout.php"><img src="img/logout.svg" alt=""></a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    }
+?>
