eb068022ac553bef74dc5c3e13ff4d802ab29ed5
smallella <ella5898g5@gmail.com>
2021-05-17 08:08:43 +0800

    update

--- facility_header.php
+++ facility_header.php
@@ -5,7 +5,7 @@
                 <img src="img/list.svg" alt="">
             </div>
         </a>
-        <a href="home.php?page=order&method=record">
+        <a href="home.php?page=order&method=records">
             <div class="a_icon_circle">
                 <img src="img/record.svg" alt="">
             </div>
@@ -23,7 +23,7 @@
 
         <div class="item_text">
             <a href="home.php?page=order">公設清單</a>
-            <a href="home.php?page=order&method=record">預約記錄</a>
+            <a href="home.php?page=order&method=records">預約記錄</a>
             <a href="home.php?page=order&method=point">點數記錄</a>
             <a href="home.php?page=order&method=repair">公設報修</a>
         </div>
