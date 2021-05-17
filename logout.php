754491974e0119c2a86c9cc59a7f529177ac1b66
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-05-17 20:22:34 +0800

    資料庫匯出的咚咚

--- logout.php
+++ logout.php
@@ -2,8 +2,8 @@
     $time = '1';
 
     session_start();
-    unset($_SESSION['auth']);
-    unset($_SESSION['id']);
+    $_SESSION = array(); 
+    session_destroy(); 
 
     echo "感謝使用!<br>".$time."秒後登出";
     header("refresh:$time;url=index.html");
