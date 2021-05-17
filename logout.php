2fdc388d829d24359510c4ed55ac5d64e4d03599
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-05-17 23:06:14 +0800

    幫忙merge

--- logout.php
+++ logout.php
@@ -2,13 +2,9 @@
     $time = '5';
 
     session_start();
-    unset($_SESSION['auth']);
-    unset($_SESSION['id']);
-?>
-<!-- =======
     $_SESSION = array(); 
     session_destroy(); 
->>>>>>> 754491974e0119c2a86c9cc59a7f529177ac1b66 -->
+?>
 <!DOCTYPE html>
 <html lang="en">
 
