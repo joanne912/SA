6e6910df6c05bb441042a4b1a5b2054ed47cbf6b
CaiHan-LIU <408401177@gapp.fju.edu.tw>
2021-05-19 00:16:58 +0800

    註冊

--- signup.php
+++ signup.php
@@ -1,6 +1,6 @@
 <?php
     $conn = include "conn.php";
-    
+    $U_NAME = $U_ADDRESS = $U_GENDER = $U_MAIL = $U_BDATE =$U_PASSWORD = $U_PHONE = $HOUSEHOLD_ID = " ";
     if(isset($_POST['signup_btn'])){
         $U_NAME = $_POST['name'];
         $U_ADDRESS = $_POST['household'];
@@ -10,20 +10,15 @@
         $U_PASSWORD = $_POST['password'];
         $U_PHONE = $_POST['phone'];
         $HOUSEHOLD_ID = $_POST['household_id'];
-        mysql_fetch_array
         $search = "SELECT * FROM household WHERE HOUSEHOLD_ID = $HOUSEHOLD_ID ";
-        if(mysql_fetch_array($search) == 1){
-            $input = array(':name' => '$U_NAME', ':household' => '$U_ADDRESS', 
-                      ':gender' => '$U_GENDER', ':mail' => '$U_MAIL', 
-                      ':bdate' => '$U_BDATE', ':password' => '$U_PASSWORD', 
-                      ':phone' => '$U_PHONE', ':household_id' => '$HOUSEHOLD_ID');
+        if(!empty($search)){
+            $input = array(':name' => '$U_NAME', ':household' =>'$U_ADDRESS', 
+            ':gender' => '$U_GENDER', ':mail' => '$U_MAIL', 
+            ':bdate' => '$U_BDATE', ':password' => '$U_PASSWORD',           ':phone' => '$U_PHONE', ':household_id' => '$HOUSEHOLD_ID');
         
-            $sql = "INSERT INTO `user`(USER_NAME, USER_ACCOUNT, USER_PASSWORD, USER_AUTHORITY) VALUES(:name, :mail, :password, 5);
-            INSERT INTO `resident`(RESIDENT_GENDER, RESIDENT_BDATE, RESIDENT_PHONE) VALUES(:gender, :bdate, :phone);
-            INSERT INTO `resident_address` VALUES(:household_id);";
+            $sql = "INSERT INTO `user`(USER_NAME, USER_ACCOUNT, USER_PASSWORD, USER_AUTHORITY) VALUES(:name, :mail, :password, 5);";
             $sth = $conn->prepared($sql);
             $sth->execute($input);
         }
-        
     }
 ?>
