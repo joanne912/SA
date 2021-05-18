38176ebbd166eeda1213e21743d59ddee1c7c252
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-05-19 01:01:23 +0800

    我覺得五秒太久了

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-    $time = '5';
+    $time = '2';
 
     session_start();
     $_SESSION = array(); 
@@ -20,7 +20,7 @@
         integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
     </script>
     <style>
-    .all{
+    .all {
         width: 100vw;
         height: 100vh;
         display: grid;
@@ -28,7 +28,8 @@
         padding: .5em 1em;
         box-sizing: border-box;
     }
-    .container{
+
+    .container {
         background-color: #f7f7fa;
         padding: 10em 0;
         border: 6px solid #f7f7fa;
@@ -37,24 +38,27 @@
         place-items: center;
         box-sizing: border-box;
     }
-    p{
+
+    p {
         width: 100%;
         font-size: 3em;
         font-weight: 700;
-        letter-spacing:  20px;
+        letter-spacing: 20px;
         text-indent: 20px;
         /* background-color: #fc6471;; */
         background-color: #fc6471;
         text-align: center;
         padding: 0;
-     
-        
+
+
     }
-    p:nth-child(1){
+
+    p:nth-child(1) {
         width: 65%;
         display: inline;
         color: white;
     }
+
     /* span{
         color: #fc6471;
         background-color: #fc6471;
@@ -62,35 +66,39 @@
         padding: 0 0 0 20px;
         margin-right: 20px;
     } */
-    p:nth-child(2){
+    p:nth-child(2) {
         background-color: transparent;
         color: #808080;
         margin: .5em 0 0;
     }
-    .col{
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-wrap: wrap;
-        }
-    .text{
+
+    .col {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .text {
         width: 70%;
         /* background-color: #c31; */
         display: flex;
         justify-content: center;
         flex-wrap: wrap;
     }
+
     @media screen and (max-width: 768px) {
         .container {
             /* height: 100vh; */
             width: 95%;
         }
-        p{
+
+        p {
             width: 100%;
             font-size: 2em;
             font-weight: 700;
-            letter-spacing: 5px;  
-            text-indent: 10px;      
+            letter-spacing: 5px;
+            text-indent: 10px;
         }
     }
 
@@ -102,29 +110,31 @@
             justify-content: center;
             margin: auto;
         }
-        p{
+
+        p {
             width: 100%;
             font-size: 1.5em;
             font-weight: 700;
-            letter-spacing: 2.5px;  
-            text-indent: 2.5px;      
+            letter-spacing: 2.5px;
+            text-indent: 2.5px;
         }
     }
     </style>
 </head>
+
 <body>
-<div class="all">
-    <div class="container">
-        <div class="col align-self-center">
-            <div class="text">
+    <div class="all">
+        <div class="container">
+            <div class="col align-self-center">
+                <div class="text">
                     <p style=" text-align:center;">感謝使用! </p>
                     <p>
                         <?php
                             echo "".$time."秒後登出";
                         ?>
                     </p>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </body>
