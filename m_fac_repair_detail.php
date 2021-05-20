9345d1c94d9096d4a2036b30babd2e3b853c9d54
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-05-21 01:29:39 +0800

    公設回復的顯示&&公設回復的新增&&讓進度回報按文字就可以選取

--- m_fac_repair_detail.php
+++ m_fac_repair_detail.php
@@ -1,9 +1,33 @@
 <?php
+    //表單回傳處理區
+    if(isset($_POST['submit'])&&isset($_POST['repair'])&&isset($_POST['facility'])&&isset($_POST['status'])){
+        $sql = "UPDATE `facilities_repair`
+        SET `FACILITIES_REPAIR_STATE` = :status , `FACILITIES_REPAIR_RETURN` = :return
+        WHERE (`FACILITIES_REPAIR_ID` = :repair
+        AND `FACILITIES_ID` = :facility
+        AND `COMMUNITY_ID` = :community);";
+        $statement = $conn->prepare($sql);
+        try{
+            $statement->execute(
+                array(
+                    ':status' => $_POST['status'],
+                    ':return' => $_POST['return'],
+                    ':repair' => $_POST['repair'],
+                    ':facility' => $_POST['facility'],
+                    ':community' => $community
+                )
+            );
+        }
+        catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+    //顯示公設報修狀況
     if(!isset($_GET['repair'])){
         header("location:home.php?page=facility&method=repair");
     }
-    $sql = "SELECT `FACILITIES_NAME`, `FACILITIES_REPAIR_CONTENT`,
-            `FACILITIES_REPAIR_STATE`, `FACILITIES_REPAIR_RETURN`
+    $sql = "SELECT `facilities_repair`.`FACILITIES_ID`, `FACILITIES_REPAIR_ID`, `FACILITIES_NAME`,
+            `FACILITIES_REPAIR_CONTENT`,`FACILITIES_REPAIR_STATE`, `FACILITIES_REPAIR_RETURN`
             FROM `facilities_repair`,`facilities` 
             WHERE ( `FACILITIES_REPAIR_ID` = ?
             AND `facilities_repair`.`FACILITIES_ID` = `facilities`.`FACILITIES_ID`
@@ -21,25 +45,33 @@
 ?>
 <link rel="stylesheet" href="css/m_repair_fac_reply.css">
 <div class="wrap">
-    <form action="home.php" >
+    <form action="home.php?page=facility&method=detail&repair=1" method="POST">
         <p><img src="img/circle.svg" alt=""> 公設名稱：<?=$row['FACILITIES_NAME']?></p>
         <hr>
         <p><img src="img/circle.svg" alt=""> 問題描述：</p>
-        <textarea name="" id="" cols="30" rows="10"><?=$row['FACILITIES_REPAIR_CONTENT']?></textarea>
+        <textarea name="content" id="" cols="30" rows="10"><?=$row['FACILITIES_REPAIR_CONTENT']?></textarea>
         <p><img src="img/circle.svg" alt=""> 進度回報：</p>
         <div class="radio_wrap">
-            <input type="radio" name="status" value="waiting" <?=($row['FACILITIES_REPAIR_STATE'] == 'waiting') ? "checked" : "" ?>>
-            <span class="radio_text">待處理</span>
-            <input type="radio" name="status" value="solving" <?=($row['FACILITIES_REPAIR_STATE'] == 'solving') ? "checked" : "" ?>>
-            <span class="radio_text">處理中</span>
-            <input type="radio" name="status" value="resolve" <?=($row['FACILITIES_REPAIR_STATE'] == 'resolve') ? "checked" : "" ?>>
-            <span class="radio_text">已處理</span>
+            <label>
+                <input type="radio" name="status" value="waiting" <?=($row['FACILITIES_REPAIR_STATE'] == 'waiting') ? "checked" : "" ?>>
+                <span class="radio_text">待處理</span>
+            </label>
+            <label>
+                <input type="radio" name="status" value="solving" <?=($row['FACILITIES_REPAIR_STATE'] == 'solving') ? "checked" : "" ?>>
+                <span class="radio_text">處理中</span>
+            </label>
+            <label>
+                <input type="radio" name="status" value="resolve" <?=($row['FACILITIES_REPAIR_STATE'] == 'resolve') ? "checked" : "" ?>>
+                <span class="radio_text">已處理</span>
+            </label>
         </div>
         <hr>
-        <p><img src="img/circle.svg" alt=""> 處理結果：<?=$row['FACILITIES_REPAIR_RETURN']?></p>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <p><img src="img/circle.svg" alt=""> 處理結果：</p>
+        <textarea name="return" id="" cols="30" rows="10"><?=$row['FACILITIES_REPAIR_RETURN']?></textarea>
         <div class="btn">
-            <input type="submit" value="確認送出">
+            <input type="hidden" name="facility" value="<?=$row['FACILITIES_ID']?>">
+            <input type="hidden" name="repair" value="<?=$row['FACILITIES_REPAIR_ID']?>">
+            <input type="submit" name="submit" value="確認送出">
         </div>
     </form>
 </div>
