8d97b3953d8f7d60fc908f46a7b8a8e155998838
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-05-20 23:31:13 +0800

    公設維修還原

--- m_fac_repair.php
+++ m_fac_repair.php
@@ -14,26 +14,23 @@
         <th>問題描述</th>
         <th>處理進度</th>
     </thead>
-    <?php
-    // 大概的後端我都寫好了，只差資料庫
-    
-    // $rs[4] = "處理中";
-        // while ($rs = mysqli_fetch_row($result)){
-        //     echo "<tr>";
-        //     echo "<a href='#'><td>" . $rs[0] . "</td></a>";
-        //     echo "<td>" . $rs[1] . "</td>";
-        //     echo "<td>" . $rs[2] . "</td>";
-            // echo "<td><a href='home.php?page=order&method=detail'>查看</a></td>";
-            // if ($rs[4] == "已處理"){
-            //     echo "<td><p class='resolve'>" . $rs[4] . "</p></td>";
-            // }
-            // else if ($rs[4] == "待處理"){
-            //     echo "<td><p class='wait'>" . $rs[4] . "</p></td>";
-            // }
-            // else if ($rs[4] == "處理中"){
-            //     echo "<td><p class='solving'>" . "$rs[4]" . "</p></td>";
-            // }
-            // echo "</tr>";
-        // }
-    ?>
+    <tr>
+        <a href='#'>
+            <td>1</td>
+        </a>
+        <td>05/20</td>
+        <td>公設名稱</td>
+        <td>
+            <a href='home.php?page=order&method=detail'>公設問題</a>
+        </td>
+        <td>
+            <p class='resolve'>已處理</p>
+        </td>
+        <td>
+            <p class='wait'></p>
+        </td>
+        <td>
+            <p class='solving'></p>
+        </td>
+    </tr>
 </table>
