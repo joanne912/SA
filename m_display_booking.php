984fe4ab069ec4bc90c4b4ef90eb47aaeeaae05b
smallella <ella5898g5@gmail.com>
2021-05-21 16:25:28 +0800

    display booking小修

--- m_display_booking.php
+++ m_display_booking.php
@@ -1,5 +1,6 @@
 
 <link rel="stylesheet" href="css/m_display_booking.css" type="text/css" />
+<link rel="stylesheet" href="css/m_facility.css" type="text/css" />
 <?php
     $sql = "SELECT `FACILITIES_ID`,`FACILITIES_NAME`,`FACILITIES_LIMIT`,`FACILITIES_IMG1`
             FROM `facilities` WHERE ( `COMMUNITY_ID` = $community );";
@@ -11,17 +12,19 @@
         foreach( $conn->query($sql) as $row2){
             ?>
             <!-- 每個獨立的公設資訊 -->
-            <div class="info_wrap"style="width:100%;">
+            <div class="info_wrap">
                 <!-- 公設圖片，連結資料庫 -->
-                <img style="border-radius:8px;width:250px;height:160px"src='<?=$row1["FACILITIES_IMG1"]?>' alt=''>
+                <div class="graphic">
+                    <img  style="border-radius:8px;width:250px;height:160px"src='<?=$row1["FACILITIES_IMG1"]?>' alt=''>
+                </div>
                 <!-- 每一公設的編號 公設icon 名稱 可容納人數 今日預約人數，需連結資料庫-->
                 <div class="textcontent" >
-                    <div>
-                        <span class="name"><?=$row1['FACILITIES_ID']?>|</span>
-                        <span class="name"><?=$row1['FACILITIES_NAME']?></span>
-                        <span>| 最大容納人數 ：<?=$row1['FACILITIES_LIMIT']?> 人</span>
-                        <span>| 今日預約人數 : <?=$row2['COUNT(`FACILITIES_ID`)']?> 人</span>
-                    </div>
+                   
+                        <h2 class="name"><?=$row1['FACILITIES_ID']?><?=$row1['FACILITIES_NAME']?></h2>
+                        <p> 最大容納人數 : <?=$row1['FACILITIES_LIMIT']?> 人
+                        <p> 今日預約人數 : <?=$row2['COUNT(`FACILITIES_ID`)']?> 人</p>
+                        
+                   
                 </div>
                 <!-- 管理員的查看住戶預約紀錄按鈕 -->
                 <div class="icon_group">
