6dcf0fce4401ae257f87c11c217d79a53b45a63a
joanne912 <83213125+joanne912@users.noreply.github.com>
2021-05-21 21:24:37 +0800

    維修進度查看

--- facility_repair_status.php
+++ facility_repair_status.php
@@ -1,1 +1,27 @@
 <?php include("facility_repair_header.php"); ?>
+
+<link rel="stylesheet" href="css/m_facilities.css">
+<link rel="stylesheet" href="css/m_repair_fac.css">
+<link rel="stylesheet" href="css/facility_repair_status.css">
+<table>
+    <thead>
+        <th>報修日期</th>
+        <th>公設名稱</th>
+        <th>位置編號</th>
+        <th>問題描述</th>
+        <th>處理進度</th>
+        <th>維修說明</th>
+    </thead>
+    <?php
+    // foreach( $conn->query($sql) as $row ){
+    //     if( $row['FACILITIES_REPAIR_STATE'] == 'resolve' ){
+    //         $state = '已處理';
+    //     }else if( $row['FACILITIES_REPAIR_STATE'] == 'waiting' ){
+    //         $state = '待處理';
+    //     }else if( $row['FACILITIES_REPAIR_STATE'] == 'solving' ){
+    //         $state = '處理中';
+    //     }
+    ?>
+    <tr>
+     
+</table>
