c23449684f425cb25f80abcf94e6e51b2128fb1d
joanne912 <83213125+joanne912@users.noreply.github.com>
2021-05-23 13:53:58 +0800

    第一優先RWD修正

--- facility_repair_add.php
+++ facility_repair_add.php
@@ -7,8 +7,20 @@
     </div>
     <hr>
     <form action="">
-        <p>公設名稱：<input type="text" name="" id="" max-length="30"></p>
-        <p>公設位置或編號：<input type="text" name="" id=""></p>
+        <p>公設名稱：
+            <select name="" id="">
+                <option value="">
+
+                </option>
+            </select>
+        </p>
+        <p>公設位置或編號：
+            <select name="" id="">
+                <option value="">
+                    
+                </option>
+            </select>
+        </p>
         <p>損壞情況：<br><textarea name="" id="" cols="50" rows="20"></textarea></p>
         <div class="btn">
             <input type="submit" value="新增報修">
