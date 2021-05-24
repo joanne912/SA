c73287c7c7f9fa772862d48c220c484e5524ad30
smallella <ella5898g5@gmail.com>
2021-05-24 23:05:34 +0800

    住戶點數紀錄完成

---
+++ facility_point.php
@@ -0,0 +1,63 @@
+
+    <link rel="stylesheet" href="css/facility_point.css">
+
+<body>
+    <div class="container">
+        <div class="outside">
+            <div class="head" >
+                <div class="name">
+                    <p>點數紀錄</p>
+                </div>
+            </div>
+            <hr class="h">
+            <div class="head" >
+                <div class="name2">
+                    <!--資料庫抓該住戶的點數-->
+                    <p>我的點數:<span class="pp">500</span><span>點</span></p>
+                </div>
+            </div>
+            <hr class="hr1">
+            <!--住戶 1.取消:退回點數 +50套用 class="add_point" 資料庫抓公設名稱 取消預約的日期 開始 結束時間 點數
+                     2.預約:扣點 -30套用 class="minor_point"-->
+            <div class="information2">
+                <span class="grayspace"><span>
+                <div class="middletext">
+                    <span> 退回點數</span>
+                    <span> 01 游泳池 </span>
+                    <span>2021/5/13</span>
+                    <span>開始:8:00</span>
+                    <span>結束:9:00</span>
+                    <span class="add_point"><font class="add_point">+</font>50</span>
+                </div>
+            </div>
+            <div class="information2">
+                <span class="grayspace"><span>
+                <div class="middletext">
+                    <span> 預約扣點</span>
+                    <span> 01 游泳池 </span>
+                    <span>2021/5/13</span>
+                    <span>開始:8:00</span>
+                    <span>結束:9:00</span>
+                    <span class="minor_point"><font class="minor_point">-</font>30
+                    </span>
+                </div>
+            </div>
+            <div class="information2">
+                <span class="grayspace"><span>
+                <div class="middletext">
+                    <span> 退回點數 </span>
+                    <span> 01 游泳池 </span>
+                    <span>2021/5/13</span>
+                    <span>開始:8:00</span>
+                    <span>結束:9:00</span>
+                    <span class="add_point"><font class="add_point">+</font>50
+                    </span>
+                </div>
+            </div>
+
+           
+        </div>
+    </div>
+</body>
+
+</html>
