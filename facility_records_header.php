a23ce2135d068a86fe43fe486635066a40fd450f
smallella <ella5898g5@gmail.com>
2021-05-27 00:25:56 +0800

    button hover & active

--- facility_records_header.php
+++ facility_records_header.php
@@ -3,7 +3,7 @@
       <!-- 顯示住戶已預約 已取消 已完成的預約紀錄 -->
     <div class="situation">
         <!-- if按下button 要加上class="" -->
-        <a href="home.php?page=facility&method=records"><input type="button" value="已預約"  class="cancle_btn press_btn"></a>
+        <a href="home.php?page=facility&method=records"><input type="button" value="已預約"  class="cancle_btn"></a>
         <a href="home.php?page=facility&method=records&to=records_cancle"><input type="button" value="已取消" class="cancle_btn"></a>
         <a href="home.php?page=facility&method=records&to=records_finish"><input type="button" value="已完成" class="cancle_btn"></a>
     </div>
