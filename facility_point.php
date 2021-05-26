75acc3b06c57b770fd7a0d028e65c1285afcf131
smallella <ella5898g5@gmail.com>
2021-05-27 00:18:33 +0800

    加入nav

--- facility_point.php
+++ facility_point.php
@@ -17,27 +17,38 @@
                 </div>
             </div>
             <hr class="hr1">
+            <br>
+            <div class="point_nav">
+                <div class="ul">
+                    <li>點數狀態</li>
+                    <li>預約公設</li>
+                    <li>預約日期</li>
+                    <li class="start">開始時間</li>
+                    <li>結束時間</li>
+                    <li>點數增減</li>
+                </div>
+            </div>
             <!--住戶 1.取消:退回點數 +50套用 class="add_point" 資料庫抓公設名稱 取消預約的日期 開始 結束時間 點數
                      2.預約:扣點 -30套用 class="minor_point"-->
             <div class="information2">
                 <span class="grayspace"><span>
                 <div class="middletext">
-                    <span> 退回點數</span>
-                    <span> 01 游泳池 </span>
+                    <span>退回點數</span>
+                    <span>01游泳池 </span>
                     <span>2021/5/13</span>
-                    <span>開始:8:00</span>
-                    <span>結束:9:00</span>
+                    <span>8:00</span>
+                    <span>9:00</span>
                     <span class="add_point"><font class="add_point">+</font>50</span>
                 </div>
             </div>
             <div class="information2">
                 <span class="grayspace"><span>
                 <div class="middletext">
-                    <span> 預約扣點</span>
-                    <span> 01 游泳池 </span>
+                    <span>預約扣點</span>
+                    <span>01游泳池 </span>
                     <span>2021/5/13</span>
-                    <span>開始:8:00</span>
-                    <span>結束:9:00</span>
+                    <span>8:00 </span>
+                    <span>9:00</span>
                     <span class="minor_point"><font class="minor_point">-</font>30
                     </span>
                 </div>
@@ -45,11 +56,11 @@
             <div class="information2">
                 <span class="grayspace"><span>
                 <div class="middletext">
-                    <span> 退回點數 </span>
-                    <span> 01 游泳池 </span>
+                    <span>退回點數</span>
+                    <span>01游泳池 </span>
                     <span>2021/5/13</span>
-                    <span>開始:8:00</span>
-                    <span>結束:9:00</span>
+                    <span>8:00</span>
+                    <span>9:00</span>
                     <span class="add_point"><font class="add_point">+</font>50
                     </span>
                 </div>
