35480179dc63e68471838ab70156ab66255db6f6
CaiHan-LIU <408401177@gapp.fju.edu.tw>
2021-05-27 14:40:25 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,7 +1,6 @@
 <?php
     $conn = include "conn.php";
-    $U_NAME = $U_ADDRESS = $U_GENDER = $U_MAIL = $U_BDATE =$U_PASSWORD = $U_PHONE = $HOUSEHOLD_ID = " ";
-
+    $U_NAME = $U_ADDRESS = $U_GENDER = $U_MAIL = $U_BDATE =$U_PASSWORD = $U_PHONE = $HOUSEHOLD_ID = $COMMUNITY_ID = " ";
     if(isset($_POST['signup_btn'])){
         $U_NAME = $_POST['name'];
         $U_ADDRESS = $_POST['household'];
@@ -13,50 +12,87 @@
         $HOUSEHOLD_ID = $_POST['household_id'];
         $COMMUNITY_ID = $_POST['community_id'];
         
-//        確認mail是否已被註冊
-        $sql1 = "SELECT `USER_MAIL` FROM `USER` WHERE `USER_MAIL`='$U_MAIL';";
+//      確認mail是否已被註冊
+        $sql1 = "SELECT `USER_ACCOUNT` FROM `USER` WHERE `USER_ACCOUNT`= ?;";
         $check_mail=$conn->prepare($sql1);
-        $row1=$check_mail->fetch(PDO::FETCH_NUM);
-        if(empty($row1[0])){
+        $check_mail->execute(array($U_MAIL));
+        $row1=$check_mail->fetch(PDO::FETCH_ASSOC);
+        
+        if(empty($row1['USER_ACCOUNT'])){
             
-//          確認戶籍代碼是否正確
-            $sql2 = "SELECT * FROM `household` WHERE `HOUSEHOLD_ID`='$HOUSEHOLD_ID';";
-            $check_id = $conn->prepare($sql2);
-            $check_id->execute(array(':household_id' => $HOUSEHOLD_ID));
-
-//          確認戶籍代碼與地址是否相符
-            $sql3 = "SELECT `HOUSEHOLD_ADDRESS` FROM `HOUSEHOLD` WHERE `HOUSEHOLD_ID`='$HOUSEHOLD_ID';";
-            $check_address=$conn->prepare($sql3);
-            $row2=$check_address->fetch(PDO::FETCH_NUM);
+//          確認社區代碼是否正確
+            $sql2 = "SELECT * FROM `community` WHERE `COMMUNITY_ID`= ?;";
+            $check_community_id = $conn->prepare($sql2);
+            $check_community_id->execute(array($COMMUNITY_ID));
+            $row2=$check_community_id->fetch(PDO::FETCH_ASSOC);
             
-            if(count($sch) == 1 && !empty($row2[0])){
-//              將資料加入user
-                $sql4 = "INSERT INTO `user`(`USER_NAME`, `USER_ACCOUNT`, `USER_PASSWORD`, `USER_AUTHORITY`) VALUES(?, ?, ?, 5);";
-                $insert_user = $conn->prepare($sql4);
-                $insert_user->execute(array($U_NAME, $U_MAIL, $U_PASSWORD));
-
-//              將資料加入resident
-                $sql5 = "INSERT INTO `resident`(`RESIDENT_GENDER`, `RESIDENT_BIRTHDAY`, `RESIDENT_PHONE`) VALUES(?, ?, ?);";
-                $insert_resident = $conn->prepare($sql5);
-                $insert_resident->execute(array($U_GENDER, $U_BDATE, $U_PHONE));
-//              將資料加入resident_address
-//                $sql6 = "INSERT INTO `resident_address`(`USER_ID`,) VALUES();";
-//                $insert_resident_address = $conn->prepare($sql6);
-//                $insert_resident_address->execute(array());
-                echo "<script>alert('註冊成功')
-                window.location.href='home.php';
-                </script>"; 
-                return false;
+            if(!empty($row2)){
+//              確認戶籍代碼是否正確
+                $sql3 = "SELECT * FROM `household` WHERE `COMMUNITY_ID`= ? AND `HOUSEHOLD_ID` = ?;";
+                $check_household_id = $conn->prepare($sql3);
+                $check_household_id->execute(array($COMMUNITY_ID, $HOUSEHOLD_ID));
+                $row3=$check_household_id->fetch(PDO::FETCH_ASSOC);
                 
-//                header("refresh:0;url=home.php");
+                if(!empty($row3)){
+
+//                  確認戶籍代碼與地址是否相符
+                    $sql4 = "SELECT `HOUSEHOLD_ADDRESS` FROM `HOUSEHOLD` WHERE `HOUSEHOLD_ID`= ?;";
+                    $check_address=$conn->prepare($sql4);
+                    $check_address->execute(array($HOUSEHOLD_ID));
+                    $row4=$check_address->fetch(PDO::FETCH_ASSOC);
+                    if(!empty($row4['HOUSEHOLD_ADDRESS']) && $row4['HOUSEHOLD_ADDRESS'] == $U_ADDRESS){
+
+                        $sql5 = "SELECT MAX(`USER_ID`) FROM `user`;";
+                        $take_uid=$conn->prepare($sql5);
+                        $take_uid->execute(array($U_MAIL));
+                        $row4=$take_uid->fetch(PDO::FETCH_ASSOC);
+                        $U_ID = $row4['MAX(`USER_ID`)'] + 1;
+
+//                      將資料加入user
+                        $sql6 = "INSERT INTO `user`(`USER_ID`, `USER_NAME`, `USER_ACCOUNT`, `USER_PASSWORD`, `USER_AUTHORITY`) VALUES(?, ?, ?, ?, 5);";
+                        $insert_user = $conn->prepare($sql6);
+                        $insert_user->execute(array($U_ID, $U_NAME, $U_MAIL, $U_PASSWORD));
+
+//                      將資料加入resident
+                        $sql7 = "INSERT INTO `resident`(`USER_ID`, `RESIDENT_GENDER`, `RESIDENT_BDATE`, `RESIDENT_PHONE`) VALUES(?, ?, ?, ?);";
+                        $insert_resident = $conn->prepare($sql7);
+                        $insert_resident->execute(array($U_ID, $U_GENDER, $U_BDATE, $U_PHONE));
+
+//                      將資料加入resident_address
+                        $sql8 = "INSERT INTO `resident_address`(`USER_ID`, `COMMUNITY_ID`, `HOUSEHOLD_ID`) VALUES(?, ?, ?);";
+                        $insert_resident = $conn->prepare($sql8);
+                        $insert_resident->execute(array($U_ID, $COMMUNITY_ID, $HOUSEHOLD_ID));
+
+                        echo "<script>alert('註冊成功')
+                        window.location.href='home.php';
+                        </script>"; 
+                        return false; 
+
+                    }else{
+                        echo "<script>alert('註冊失敗, 戶籍代碼與住址不符')
+                        window.location.href='index.html';
+                        </script>"; 
+                        return false;
+                    }
+                }else{
+                    echo "<script>alert('註冊失敗, 戶籍代碼輸入錯誤')
+                    window.location.href='index.html';
+                    </script>"; 
+                    return false;
+                }
+
             }else{
-                echo "<script>alert('註冊失敗')
+                echo "<script>alert('註冊失敗, 社區代碼輸入錯誤')
                 window.location.href='index.html';
                 </script>"; 
                 return false;
-                
-//                header("refresh:0;url=index.html");
-            }
+            }             
+        
+        }else{
+            echo "<script>alert('註冊失敗, 此帳號已被註冊')
+            window.location.href='index.html';
+            </script>"; 
+            return false;
         }
     }
 ?>
