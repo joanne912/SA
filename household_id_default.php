bc90b106fe1b5c5910a0919b4f52e4fe84570697
joanne912 <83213125+joanne912@users.noreply.github.com>
2021-05-31 03:10:35 +0800

    住戶代碼詳細內容css已加好

--- household_id_default.php
+++ household_id_default.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="css/household_id_default.css">
+
 <?php
     if(!isset($_GET['household_id'])){
         header("location:home.php?page=householdid");
@@ -10,31 +12,35 @@
     $row = $statement->fetch(PDO::FETCH_ASSOC);
 ?>
 
-<table>
-    <tr>
-        <th>戶別</th>
-        <th><font size=3 color=red><?=$row['HOUSEHOLD_ADDRESS']?></font></th>
-        <th>戶籍代碼</th>
-        <th><font size=3 color=red><?=$_GET['household_id']?></font></th>
-    </tr>
-    <tr>
-        <td colspan="2">姓名</td>
-        <td colspan="2">電話</td>
-    </tr>
-    <?php
-        $sql = "SELECT `USER_NAME`,`RESIDENT_PHONE` FROM `user`,`resident`,`resident_address`
-        WHERE ( `user`.`USER_ID` = `resident`.`USER_ID`
-        AND `user`.`USER_ID` = `resident_address`.`USER_ID` 
-        AND `COMMUNITY_ID` = $community 
-        AND `HOUSEHOLD_ID` = $id);";
-        $result = $conn->query($sql);
-        while($row = $result->fetch(PDO::FETCH_ASSOC) ){
-    ?>
-    <tr>
-        <td colspan='2'><?=$row['USER_NAME']?></td>
-        <td colspan='2'><?=$row['RESIDENT_PHONE']?></td>
-    </tr>
-    <?php
-        }
-    ?>
-</table>
+<div class="cont">
+    <table>
+        <thead>
+            <th>戶別</th>
+            <th><font size=3 color=red><?=$row['HOUSEHOLD_ADDRESS']?></font></th>
+            <th>戶籍代碼</th>
+            <th><font size=3 color=red><?=$_GET['household_id']?></font></th>
+        </thead>
+    </table>
+    <table>
+        <tr>
+            <th colspan="2" class="set">姓名</th>
+            <th colspan="2" class="set">電話</th>
+        </tr>
+        <?php
+            $sql = "SELECT `USER_NAME`,`RESIDENT_PHONE` FROM `user`,`resident`,`resident_address`
+            WHERE ( `user`.`USER_ID` = `resident`.`USER_ID`
+            AND `user`.`USER_ID` = `resident_address`.`USER_ID` 
+            AND `COMMUNITY_ID` = $community 
+            AND `HOUSEHOLD_ID` = $id);";
+            $result = $conn->query($sql);
+            while($row = $result->fetch(PDO::FETCH_ASSOC) ){
+        ?>
+        <tr>
+            <td colspan='2'><?=$row['USER_NAME']?></td>
+            <td colspan='2'><?=$row['RESIDENT_PHONE']?></td>
+        </tr>
+        <?php
+            }
+        ?>
+    </table>
+</div>
