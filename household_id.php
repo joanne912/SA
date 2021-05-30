91fc831b7cfdcc255f43e52ea556e914b1190672
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-05-30 17:05:23 +0800

    Update household_id.php

--- household_id.php
+++ household_id.php
@@ -1,58 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    $searchtxt = "";
+    if( isset( $_POST["searchtxt"] ) ) {
+        $searchtxt = $_POST["searchtxt"];
+        $sql = "SELECT `HOUSEHOLD_ADDRESS`,`COMMUNITY_ID`,`HOUSEHOLD_ID` FROM `household` WHERE `HOUSEHOLD_ADDRESS` LIKE ? ;";
+    }
+    else{
+        $sql = "SELECT `HOUSEHOLD_ADDRESS`,`COMMUNITY_ID`,`HOUSEHOLD_ID` FROM `household`;"; //預設搜尋的SQL字串 
+    }
+    $statement = $conn->prepare($sql);
+    $statement->execute(array($searchtxt));
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- <link rel="stylesheet" href="css/p.css" type="text/css" /> -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
-    </script>
-    <style>
-        /* .top{
-            width:100vw;
-            height: 20vh;
-            background-color: #ffc;
-        }
-        .bottom{
-            width:100vw;
-            height: 80vh;
-            background-color: red;
-        } */
-    </style>
-</head>
-<body>  
-    <form action="home.php?page=householdid" method=POST>
-	<!-- <input type=hidden name="method" value="query"> 現在method變數值是query，避免搜尋後值消失，所以用hidden再傳一次值 -->
-	<p align=center>請輸入戶別:
-        <input type=text name="searchtxt" value=<?php echo $searchtxt; ?>>
-        <input type=submit value="搜尋戶籍戶別">
-    </p>
-	</form>
-    <table class="table table-hover">
-	<tr class="warning"><td>戶別</td><td>戶籍代碼</td><td>查看</td></tr>
-    <?php	
-        $conn = include "conn.php";
-        $searchtxt = $_POST["searchtxt"];  
-        if(empty($searchtxt))
-            {
-            $sql="SELECT * FROM `household`"; //預設搜尋的SQL字串
-            }
-        else
-            {
-            $sql="SELECT * FROM `household` WHERE `HOUSEHOLD_ADDRESS` LIKE '%".$searchtxt."%'"; 
-            }
-        $search=$conn->query($sql);
-        while($record=$search->fetch())
-        {
-            echo "<tr><td>$record[2]</td><td>$record[1]</td>";
-            echo "<td><a href=home.php?page=householdid&method=look&household_id=$record[0]>[查看]</a></td></tr>"; 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+</script>
+
+<form action="home.php?page=householdid" method=POST>
+<p align=center>請輸入戶別:
+    <input type=text name="searchtxt" value=<?php echo $searchtxt; ?>>
+    <input type=submit value="搜尋戶籍戶別">
+</p>
+</form>
+<table class="table table-hover">
+    <tr class="warning">
+        <td>戶別</td>
+        <td>戶籍代碼</td>
+        <td>查看</td>
+    </tr>
+    <?php
+        while( $row = $statement->fetch(PDO::FETCH_ASSOC) ) {
+    ?>
+    <tr>
+        <td><?=$row['HOUSEHOLD_ADDRESS']?></td>
+        <td><?=$row['COMMUNITY_ID']?></td>
+        <td><a href="home.php?page=householdid&method=look&household_id=<?=$row['HOUSEHOLD_ID']?>">[查看]</a></td>
+    </tr>
+    <?php
         }
     ?>
-    </table>
-</body>
-</html>
+</table>
