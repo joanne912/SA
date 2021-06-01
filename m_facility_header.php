29317c7cae3854fd5aa3e8c628a47d4322312b57
smallella <ella5898g5@gmail.com>
2021-06-01 14:20:48 +0800

    改小東西

--- m_facility_header.php
+++ m_facility_header.php
@@ -1,3 +1,13 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/f_reserve.js"></script>
+<style>
+.item_text a, td a, .btn_wrap a{
+    color: #808080 !important;
+    text-decoration: none !important;
+} 
+</style>
 <link rel="stylesheet" href="css/m_facilities.css">
 <div class="act_wrap">
     <a href="home.php?page=facility">
