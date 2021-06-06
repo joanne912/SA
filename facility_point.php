036bdfc0d53e62aeaaca90bb0b4eefc00043ab40
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-06-06 12:09:32 +0800

    點數查看

--- facility_point.php
+++ facility_point.php
@@ -1,3 +1,8 @@
+<?php
+    $sql = "SELECT `POINT` FROM `household` 
+    WHERE `HOUSEHOLD_ID` = $household AND `COMMUNITY_ID` = $community;";
+    $row = $conn->query($sql)->fetch(PDO::FETCH_ASSOC);
+?>
 <link rel="stylesheet" href="css/facility_point.css">
 <body>
     <div class="container">
@@ -11,7 +16,7 @@
             <div class="head" >
                 <div class="name2">
                     <!--資料庫抓該住戶的點數-->
-                    <p>我的點數:<span class="pp">500</span><span>點</span></p>
+                    <p>我的點數:<span class="pp"><?=$row['POINT']?></span><span>點</span></p>
                 </div>
             </div>
             <hr class="hr1">
@@ -21,50 +26,33 @@
                     <li>點數狀態</li>
                     <li>預約公設</li>
                     <li>預約日期</li>
-                    <li class="start">開始時間</li>
-                    <li>結束時間</li>
                     <li>點數增減</li>
                 </div>
             </div>
-            <!--住戶 1.取消:退回點數 +50套用 class="add_point" 資料庫抓公設名稱 取消預約的日期 開始 結束時間 點數
-                     2.預約:扣點 -30套用 class="minor_point"-->
-            <div class="information2">
-                <span class="grayspace"><span>
-                <div class="middletext">
-                    <span>退回點數</span>
-                    <span>01游泳池 </span>
-                    <span>2021/5/13</span>
-                    <span>8:00</span>
-                    <span>9:00</span>
-                    <span class="add_point"><font class="add_point">+</font>50</span>
-                </div>
-            </div>
-            <div class="information2">
-                <span class="grayspace"><span>
-                <div class="middletext">
-                    <span>預約扣點</span>
-                    <span>01游泳池 </span>
-                    <span>2021/5/13</span>
-                    <span>8:00 </span>
-                    <span>9:00</span>
-                    <span class="minor_point"><font class="minor_point">-</font>30
-                    </span>
-                </div>
-            </div>
-            <div class="information2">
-                <span class="grayspace"><span>
-                <div class="middletext">
-                    <span>退回點數</span>
-                    <span>01游泳池 </span>
-                    <span>2021/5/13</span>
-                    <span>8:00</span>
-                    <span>9:00</span>
-                    <span class="add_point"><font class="add_point">+</font>50
-                    </span>
-                </div>
-            </div>
-
-           
+            <!--住戶1.取消:退回點數 +50套用 class="add_point" 資料庫抓公設名稱 取消預約的日期 點數
+                    2.預約:扣點 -30套用 class="minor_point"-->
+            <?php
+                $sql = "SELECT `facilities`.`FACILITIES_ID`,`FACILITIES_NAME`,
+                        `FACILITIES_BOOKING_DATE`,`FACILITIES_POINT`
+                        FROM `facilities`,`facilities_booking`
+                        WHERE `facilities`.`FACILITIES_ID` = `facilities_booking`.`FACILITIES_ID`
+                        AND `HOUSEHOLD_ID` = $household AND `facilities`.`COMMUNITY_ID` = $community
+                        AND `IS_CANCEL` = 0 ORDER BY `FACILITIES_BOOKING_DATE` DESC;";
+                foreach( $conn->query($sql) as $row ){
+                    ?>
+                    <div class="information2">
+                        <span class="grayspace"><span>
+                        <div class="middletext">
+                            <span>預約扣點</span>
+                            <span><?=$row['FACILITIES_ID']?><?=$row['FACILITIES_NAME']?></span>
+                            <span><?=$row['FACILITIES_BOOKING_DATE']?></span>
+                            <span class="minor_point"><font class="minor_point">-</font><?=$row['FACILITIES_POINT']?>
+                            </span>
+                        </div>
+                    </div>
+                    <?php
+                }
+            ?>
         </div>
     </div>
 </body>
