bf7eec664014f6bcafd49cf7d14c90fad3be3078
ShihMin-Chien <jason8992825@gmail.com>
2021-06-09 02:03:44 +0800

    增加hover

--- facility_repair_header.php
+++ facility_repair_header.php
@@ -1,8 +1,8 @@
 <link rel="stylesheet" href="css/facility_repair_header.css">
 <body>
     <div class="btn_wrap">
-        <a href="home.php?page=facility&method=repair_add">公設報修</a>
-        <a href="home.php?page=facility&method=repair_add&to=repair_status">維修進度</a>
+        <a href="home.php?page=facility&method=repair_add" class="page_hover">公設報修</a>
+        <a href="home.php?page=facility&method=repair_add&to=repair_status" class="page_hover">維修進度</a>
     </div>
 </body>
 
