1f2554b291a100cc46b96ac63193ae8264aabc93
joanne912 <83213125+joanne912@users.noreply.github.com>
2021-06-10 02:12:31 +0800

    移除公社清單的公設編號

--- facility_records.php
+++ facility_records.php
@@ -30,7 +30,7 @@
             </div>
             <!-- 每一住戶預約公設的編號 名稱 日期 時段 ，需連結資料庫 -->
             <div class="text">
-                <h4><?=$row['FACILITIES_ID']?> <?=$row['FACILITIES_NAME']?></h4> 
+                <h4><?=$row['FACILITIES_NAME']?></h4> 
                 <h5>預約日期：<span><?=$row['FACILITIES_BOOKING_DATE']?></span></h5>
                 <h5>預約時段：<span><?=$time['MIN(`FACILITIES_START`)']?> : 00 <span>~</span> <?=$time['MAX(`FACILITIES_START`)']?> : 00</span></h5>
             </div>
--- facility_records_cancle.php
+++ facility_records_cancle.php
@@ -24,7 +24,7 @@
             </div>
             <!-- 每一住戶預約公設的編號 名稱 日期 時段 ，需連結資料庫 -->
             <div class="text">
-                <h4 class="h4"><?=$row['FACILITIES_ID']?> <?=$row['FACILITIES_NAME']?></h4> 
+                <h4 class="h4"><?=$row['FACILITIES_NAME']?></h4> 
                 <h7 class="h7">預約日期：<span><?=$row['FACILITIES_BOOKING_DATE']?></span></h7><br>
                 <h7 class="h7">預約時段：<span><?=$time['MIN(`FACILITIES_START`)']?> : 00 <span>~</span> <?=$time['MAX(`FACILITIES_START`)']?> : 00</span></h7>
             </div>
