d743dbcd438d09df4214a0ecbd9639863fbc23cb
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-06-09 20:44:36 +0800

    社區管理員搜尋戶別的bug

--- household_id.php
+++ household_id.php
@@ -1,13 +1,13 @@
 <?php
-    @$searchtxt = "";
+    $searchtxt = "";
     $sql = "SELECT `HOUSEHOLD_ADDRESS`,`COMMUNITY_ID`,`HOUSEHOLD_ID` 
                 FROM `household` WHERE `COMMUNITY_ID` = $community"; //預設搜尋的SQL字串
     if( isset( $_POST["searchtxt"] ) ) {
         $searchtxt = $_POST["searchtxt"];
-        $sql += " AND `HOUSEHOLD_ADDRESS` LIKE ? ";
+        $sql .= " AND `HOUSEHOLD_ADDRESS` LIKE ? ";
     }
     $statement = $conn->prepare($sql);
-    $statement->execute(array($searchtxt));
+    $statement->execute(array("%".$searchtxt."%"));
 ?>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
@@ -16,7 +16,7 @@
     integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
 </script>
 <link rel="stylesheet" href="css/household_id.css">
-<form class="search" action="household_id.php">
+<form class="search" method="POST" action="home.php?page=householdid">
     <label for="searchtxt">請輸入戶別 :</label> 
     <input class="input1" type="text" name="searchtxt" id="searchtxt" value=<?php echo $searchtxt; ?>>
     <input class="input2" type="submit" value="搜尋戶籍戶別">
