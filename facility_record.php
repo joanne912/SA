1d6f4cc3f39c015900541a968bb7d1b2061a0f51
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-06-10 10:38:20 +0800

    預約時段更正

--- facility_record.php
+++ facility_record.php
@@ -50,7 +50,7 @@
     <!--從資料庫匯出(顯示住戶預約資訊)該住戶預約該公設的名稱 日期 時段 人數 電話 email 原本點數 剩餘點數 若有借用設備顯示借用的設備名稱 -->
     <p class="dot"><img src="img/circle.svg">&nbsp公設名稱 : <?=$row['FACILITIES_NAME']?></img></p>
     <div class="information">
-        <p class="content">預約時段 :  <span><?=$time['MIN(`FACILITIES_START`)']?> : 00 <span>~</span> <?=$time['MAX(`FACILITIES_START`)']?> : 00</span></p>
+        <p class="content">預約時段 :  <span><?=$time['MIN(`FACILITIES_START`)']?> : 00 <span>~</span> <?=$time['MAX(`FACILITIES_START`)']+1?> : 00</span></p>
         <p class="content">預約日期 :  <?=$row['FACILITIES_BOOKING_DATE']?></p>
         <p class="content">預約人數 : <span><?=$row['FACILITIES_BOOKING_AMOUNT']?><span><span>人<span></p>
         <p class="content">本次點數 : <span class="yourpoint"><?=$row['FACILITIES_POINT']?></span> 點</p>
